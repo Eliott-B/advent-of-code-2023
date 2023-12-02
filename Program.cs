5e233c2de6732427d71bff14382d839b5471e5dc
Eliott-B <eliottb.info@gmail.com>
2023-12-02 18:45:17 +0100

    Day 1 Part 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Advent of code 2023");
+﻿using AdventOfCode2023.day;
+
+string szProjectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+Console.WriteLine("Advent of code 2023");
+
+// DAY 1
+string szInputDay1 = szProjectPath + "/input/day1-1";
+int iResult = day1.step1(szInputDay1);
+Console.WriteLine(iResult);
+
+Console.ReadKey();
---
+++ day/day1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace AdventOfCode2023.day
+{
+	public static class day1
+	{
+		public static int step1(string szInputPath)
+		{
+			string szInput = File.ReadAllText(szInputPath);
+			string[] szLines = szInput.Split("\n");
+			List<List<string>> szNumber = new List<List<string>>();
+			int iIndex = 0;
+			foreach (string szLine in szLines)
+			{
+				bool bFirst = true;
+				foreach (char szChar in szLine)
+				{
+					if (szChar >= 48 &&
+						szChar <= 57)
+					{
+                        if (bFirst == true)
+                        {
+							szNumber.Add(new List<string>());
+                            szNumber[iIndex].Add(szChar.ToString());
+                            szNumber[iIndex].Add(szChar.ToString());
+                            bFirst = false;
+                        }
+                        else
+                        {
+                            szNumber[iIndex][1] = szChar.ToString();
+                        }
+                    }
+				}
+				iIndex++;
+            }
+
+			int iSum = 0;
+			foreach (List<string> szChars in szNumber)
+			{
+				iSum += Int32.Parse(szChars[0] + szChars[1]);
+			}
+
+			return iSum;
+        }
+    }
+}
